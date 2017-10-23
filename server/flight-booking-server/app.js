890c8f9d71358746a796bce8981d1d4e5829e1a6
Quy Tran <quytran288@gmail.com>
2017-10-23 11:31:21 -0500

    Allow Cross Origin

--- server/flight-booking-server/app.js
+++ server/flight-booking-server/app.js
@@ -26,6 +26,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
+
 // Custom setup
 app.enable('case sensitive routing');
 app.set('x-powered-by', false);

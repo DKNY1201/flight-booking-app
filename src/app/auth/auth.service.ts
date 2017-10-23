97fb8fb6d5fccd5d0e1ca322502768d3f13c2757
Quy Tran <quytran288@gmail.com>
2017-10-23 18:09:51 -0500

    Check email already existing. Show day, month, year in Signup page

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,14 +2,20 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {User} from "../models/user.model";
 import {Utils} from "../shared/Utils";
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class AuthService {
   userUrl = Utils.SERVER_USER_URL;
+  checkEmailUrl = Utils.SERVER_CHECK_EMAIL_URL;
 
   constructor(private httpClient: HttpClient) {}
 
   signup(user: User) {
     return this.httpClient.post(this.userUrl, user);
+  }
+
+  checkEmail(email: string): Observable<any> {
+    return this.httpClient.get(`${this.checkEmailUrl}/${email}`);
   }
 }
--- src/app/shared/Utils.ts
+++ src/app/shared/Utils.ts
@@ -1,23 +1,26 @@
 import {FormControl} from "@angular/forms";
 import {Observable} from "rxjs/Observable";
+import {HttpClient} from "@angular/common/http";
 
 import 'rxjs/Rx';
 
-import 'rxjs/add/operator/do';
-import {HttpClient} from "@angular/common/http";
-
 export class Utils {
-
   constructor(private httpClient: HttpClient) {}
 
   static SERVER_URL = 'http://localhost:3000';
   static SERVER_USER_URL = Utils.SERVER_URL + '/users';
+  static SERVER_CHECK_EMAIL_URL = Utils.SERVER_USER_URL + '/checkemail';
 
-  static validateUniqueEmail(control: FormControl): Observable<any> | Promise<any> {
-    if (control.value === "P123") {
-      return Observable.of({uniqueProductID: true});
-    } else {
-      return Observable.of(null);
+  static DAYS = Array(31).fill(0).map((x, i) => i + 1);
+  static MONTHS = Array(12).fill(0).map((x, i) => i + 1);
+  static get YEARS() {
+    const startYear = 1920;
+    const endYear = 2000;
+    const years = [];
+    for (let i = startYear; i <= endYear; i++) {
+      years.push(i);
     }
+
+    return years;
   }
 }

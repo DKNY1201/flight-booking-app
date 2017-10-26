ed5494211b6a6d869b5bee5765c40b0496ac54d0
Quy Tran <quytran288@gmail.com>
2017-10-26 08:21:14 -0500

    Reduce number of passenger after booking

--- server/flight-booking-server/routes/bookings.js
+++ server/flight-booking-server/routes/bookings.js
@@ -2,76 +2,91 @@ const express = require('express');
 const router = express.Router();
 
 const Booking = require('../models/booking');
+const Itinerary = require('../models/itinerary');
 
 function save(response, booking) {
-    booking.save((err, result) => {
-        if (err) {
-            response.status(500).json(err);
-        }
-        else {
-            response.json(booking);
-        }
-    });
+  booking.save((err, result) => {
+    if (err) {
+      response.status(500).json(err);
+    }
+    else {
+      response.json(booking);
+    }
+  });
 }
 
 router.get('/:id', function (req, res, next) {
-    Booking.findById(req.params.id, (err, booking) => {
-        if (err) {
-            res.status(500).json(err);
-        }
-        else if (booking) {
-            res.json(booking);
-        }
-        else {
-            res.status(404).json("The booking does not exist.");
-        }
-    });
+  Booking.findById(req.params.id, (err, booking) => {
+    if (err) {
+      res.status(500).json(err);
+    }
+    else if (booking) {
+      res.json(booking);
+    }
+    else {
+      res.status(404).json("The booking does not exist.");
+    }
+  });
 });
 
 router.post('/', function (req, res, next) {
-    var booking = new Booking({
-        userId: req.body.userId,
-        itineraryId: req.body.itineraryId,
-        departure: req.body.departure,
-        departureDate: req.body.departureDate,
-        departureTime: req.body.departureTime,
-        arrival: req.body.arrival,
-        arrivalDate: req.body.arrivalDate,
-        arrivalTime: req.body.arrivalTime,
-        passenger: req.body.passenger,
-        card: req.body.card,
-        billing: req.body.billing,
-        confirmationCode: `EF${new Date().getTime()}`
-    });
-    save(res, booking);
+  var booking = new Booking({
+    userId: req.body.userId,
+    itineraryId: req.body.itineraryId,
+    departure: req.body.departure,
+    departureDate: req.body.departureDate,
+    departureTime: req.body.departureTime,
+    arrival: req.body.arrival,
+    arrivalDate: req.body.arrivalDate,
+    arrivalTime: req.body.arrivalTime,
+    passenger: req.body.passenger,
+    card: req.body.card,
+    billing: req.body.billing,
+    confirmationCode: `EF${new Date().getTime()}`
+  });
+  Itinerary.findOneAndUpdate(
+    {_id: booking.itineraryId},
+    {$inc: {passenger: -1}}, (err, itinerary) => {
+      if (err) {
+        return res.status(500).json({
+          title: 'Something went wrong, check the error below',
+          error: err
+        });
+      }
+
+      save(res, booking);
+    }
+  )
+
+
 });
 
 router.put('/:id', function (req, res, next) {
-    Booking.findByIdAndUpdate(req.params.id, (err, booking) => {
-        if (err) {
-            res.status(500).json(err);
-        }
-        else if (booking) {
-            booking.itineraryId = req.body.itineraryId;
-            booking.date = req.body.date;
-            booking.note = req.body.note;
-            save(res, booking);
-        }
-        else {
-            res.status(404).json("The booking does not exist.");
-        }
-    });
+  Booking.findByIdAndUpdate(req.params.id, (err, booking) => {
+    if (err) {
+      res.status(500).json(err);
+    }
+    else if (booking) {
+      booking.itineraryId = req.body.itineraryId;
+      booking.date = req.body.date;
+      booking.note = req.body.note;
+      save(res, booking);
+    }
+    else {
+      res.status(404).json("The booking does not exist.");
+    }
+  });
 });
 
 router.delete('/:id', function (req, res, next) {
-    Booking.findByIdAndRemove(req.params.id, (err, booking) => {
-        if (err) {
-            res.status(500).json(err);
-        }
-        else {
-            res.json(true);
-        }
-    });
+  Booking.findByIdAndRemove(req.params.id, (err, booking) => {
+    if (err) {
+      res.status(500).json(err);
+    }
+    else {
+      res.json(true);
+    }
+  });
 });
 
 module.exports = router;

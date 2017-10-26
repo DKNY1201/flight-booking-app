e62d4e3881b32aa3ec9b8a429d45ccee6ad418c2
Lam Nguyen <qlnguyen@mum.edu>
2017-10-26 11:16:55 -0500

    Add credit card pipe to booking history detail page.
    -Add credit card pipe to booking history detail page.
    -Add iternary header to booking history detail page.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,23 +5,20 @@ import {HoverDirective} from "./hover.directive";
 import {DropdownDirective} from "./dropdown.directive";
 import {LimitPipe} from "./limit.pipe";
 import {CapitalizePipe} from "./capitalize.pipe";
-import {CreditCard} from "./creditcard.pipe";
 
 @NgModule({
   declarations: [
     HoverDirective,
     DropdownDirective,
     LimitPipe,
-    CapitalizePipe,
-    CreditCard
+    CapitalizePipe
   ],
   exports: [
     CommonModule,
     HoverDirective,
     DropdownDirective,
     LimitPipe,
-    CapitalizePipe,
-    CreditCard
+    CapitalizePipe
   ]
 })
 export class SharedModule {}

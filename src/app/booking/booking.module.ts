ed143193177d36a65b60fc2476c28960b5be8ad2
Lam Nguyen <qlnguyen@mum.edu>
2017-10-25 23:08:43 -0500

    Update booking form.
    Update booking form.

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from "@angular/common";
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { BookingRoutingModule } from './booking-routing.module';
 import { BookingComponent } from './booking.component';
+import { NameTitlePipe } from './name-title.pipe';
 import { BookingService } from './booking.service';
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
         BookingRoutingModule
     ],
     exports: [
-        BookingComponent
+        BookingComponent,
+        NameTitlePipe
     ],
     declarations: [
-        BookingComponent
+        BookingComponent,
+        NameTitlePipe
     ],
     providers: [
         BookingService
